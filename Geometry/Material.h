11feab104a3731bceca2b7b13b61fb314672a81c
saguillo2000 <oriolsaguilo@gmail.com>
2021-05-01 20:44:56 +0200

    Dependencies to Material same as Object and Components of Material.h

--- Geometry/Material.h
+++ Geometry/Material.h
@@ -1,7 +1,26 @@
 #pragma once
 
-#include <QGLShaderProgram>
+#include <QObject>
+#include <vector>
 #include <memory>
+
+#include <library/Common.h>
+using namespace Common;
+
+#include <QGLShaderProgram>
+#include <QOpenGLTexture>
+
+#include <QFile>
+#include <QRegularExpression>
+#include <QDebug>
+#include <QString>
+#include <QStringList>
+
+#include <Geometry/TG.h>
+#include <Geometry/Animation.h>
+#include <Geometry/Cara.h>
+
+typedef vec3  point3;
 
 using namespace std;
 // TO DO: A canviar a la fase 1 de la practica 2
@@ -13,6 +32,11 @@ public:
     Material();
 
     void toGPU(shared_ptr<QGLShaderProgram> program);
+private:
+    vec3 ambiental;
+    vec3 diffuse;
+    vec3 specular;
+    float shiness;
 
 };
 
