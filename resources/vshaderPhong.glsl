0b6a8155ad9cd8a859d07a1945f1270d1075088f
saguillo2000 <oriolsaguilo@gmail.com>
2021-05-16 12:40:00 +0200

    Program List and able to change pair of shaders Gouraud, Phong, Toon

--- resources/vshaderPhong.glsl
+++ resources/vshaderPhong.glsl
@@ -1,7 +1,7 @@
 #version 330
 
 layout (location = 0) in vec4 vPosition;
-layout (location = 1) in vec4 vColor;
+layout (location = 1) in vec4 vNormal;
 
 uniform mat4 model_view;
 uniform mat4 projection;
@@ -12,5 +12,5 @@ void main()
 {
     gl_Position = projection*model_view*vPosition;
     gl_Position = gl_Position/gl_Position.w;
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    color = vec4(0.0, 1.0, 0.0, 1.0);
 }
