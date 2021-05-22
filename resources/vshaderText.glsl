62cb504b64a8cb65b2148ea32c8dd4ccd783c17e
angelmc1309 <angelmc1309@gmail.com>
2021-05-22 12:24:06 +0200

    Textures with Phong.

--- resources/vshaderText.glsl
+++ resources/vshaderText.glsl
@@ -9,14 +9,13 @@
 
 
 layout (location = 0) in vec4 vPosition;
-layout (location = 1) in vec4 vNormal;
+layout (location = 1) in vec4 vColor;
 layout (location = 2) in vec2 vCoordTexture;
 
 uniform mat4 model_view;
 uniform mat4 projection;
 
-out vec4 Normal;
-out vec4 Position;
+out vec4 color;
 out vec2 v_texcoord;
 
 
@@ -25,8 +24,7 @@ void main()
 {
   gl_Position = projection*model_view*vPosition;
   gl_Position = gl_Position/gl_Position.w;
-  Normal = vNormal;
-  Position = vPosition;
+  color = vColor;
   // Pas de les coordenades de textura al fragment shader
   // El valor del color i les coordenades de textura s'interpolaran automaticament
   // en els fragments interiors a les cares dels polígons
